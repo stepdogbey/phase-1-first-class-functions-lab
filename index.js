7d3b336848a054034c05b0d684675c5540b6f96a
stephen <stevedogbey@gmail.com>
2022-06-21 23:01:24 +0000

    function lap completed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function(){
+    let newArray = ['Antonia', 'Nuru'];
+    return newArray;
+}
+returnFirstTwoDrivers();
+
+const returnLastTwoDrivers = function(){
+    let anotherArray = ['Amari', 'Mo'];
+    return anotherArray;
+
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (someValue){
+    return function(someValue1){
+     return someValue*someValue1;
+    }
+
+}
+
+const fareDoubler  = createFareMultiplier(2);
+fareDoubler(10);
+
+const fareTripler  = createFareMultiplier(3);
+fareTripler(12);
+
+function selectDifferentDrivers(drivers, selectDifferentDrivers){
+    if(selectDifferentDrivers === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(drivers);
+
+    }else{
+        return returnLastTwoDrivers(drivers);
+    }
+}
